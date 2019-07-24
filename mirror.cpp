6ba4fda42f49367ca3603e093b80a999fa46a7bb
Remi minitop <remi@uselessworks.com>
2019-07-24 21:57:06 +0200

    basic nested reflection

--- mirror.cpp
+++ mirror.cpp
@@ -8,17 +8,33 @@ namespace mirror
 {
 	ClassSet	g_classSet;
 
-	mirror::MemberType getMemberType(int8_t*) { return MemberType_int8; }
-	mirror::MemberType getMemberType(int16_t*) { return MemberType_int16; }
-	mirror::MemberType getMemberType(int32_t*) { return MemberType_int32; }
-	mirror::MemberType getMemberType(int64_t*) { return MemberType_int64; }
-	mirror::MemberType getMemberType(uint8_t*) { return MemberType_uint8; }
-	mirror::MemberType getMemberType(uint16_t*) { return MemberType_uint16; }
-	mirror::MemberType getMemberType(uint32_t*) { return MemberType_uint32; }
-	mirror::MemberType getMemberType(uint64_t*) { return MemberType_uint64; }
-	mirror::MemberType getMemberType(float*) { return MemberType_float; }
-	mirror::MemberType getMemberType(double*) { return MemberType_double; }
-	mirror::MemberType getMemberType(std::string*) { return MemberType_string; }
+	mirror::MemberType GetMemberType(bool&) { return MemberType_bool; }
+	mirror::MemberType GetMemberType(char&) { return MemberType_char; }
+	mirror::MemberType GetMemberType(int8_t&) { return MemberType_int8; }
+	mirror::MemberType GetMemberType(int16_t&) { return MemberType_int16; }
+	mirror::MemberType GetMemberType(int32_t&) { return MemberType_int32; }
+	mirror::MemberType GetMemberType(int64_t&) { return MemberType_int64; }
+	mirror::MemberType GetMemberType(uint8_t&) { return MemberType_uint8; }
+	mirror::MemberType GetMemberType(uint16_t&) { return MemberType_uint16; }
+	mirror::MemberType GetMemberType(uint32_t&) { return MemberType_uint32; }
+	mirror::MemberType GetMemberType(uint64_t&) { return MemberType_uint64; }
+	mirror::MemberType GetMemberType(float&) { return MemberType_float; }
+	mirror::MemberType GetMemberType(double&) { return MemberType_double; }
+	mirror::MemberType GetMemberType(std::string&) { return MemberType_string; }
+
+	mirror::Class* GetClass(bool&) { return nullptr; }
+	mirror::Class* GetClass(char&) { return nullptr; }
+	mirror::Class* GetClass(int8_t&) { return nullptr; }
+	mirror::Class* GetClass(int16_t&) { return nullptr; }
+	mirror::Class* GetClass(int32_t&) { return nullptr; }
+	mirror::Class* GetClass(int64_t&) { return nullptr; }
+	mirror::Class* GetClass(uint8_t&) { return nullptr; }
+	mirror::Class* GetClass(uint16_t&) { return nullptr; }
+	mirror::Class* GetClass(uint32_t&) { return nullptr; }
+	mirror::Class* GetClass(uint64_t&) { return nullptr; }
+	mirror::Class* GetClass(float&) { return nullptr; }
+	mirror::Class* GetClass(double&) { return nullptr; }
+	mirror::Class* GetClass(std::string&) { return nullptr; }
 
 #define OFFSET_BASIS	2166136261
 #define FNV_PRIME		16777619
@@ -56,53 +72,75 @@ namespace mirror
 
 	}
 
-	Class::Class(const char* _name)
-		: m_name(_name)
+	ClassMember::ClassMember(const char* _name, size_t _offset, MemberType _type, Class* _subClass)
+		: name(_name)
+		, offset(_offset)
+		, type(_type)
+		, subClass(_subClass)
 	{
 
 	}
 
-	void Class::addMember(const char* _name, size_t _address, MemberType _type)
+	Class::Class(const char* _name, size_t _typeHash)
+		: m_typeHash(_typeHash)
+		, m_name(_name)
+	{
+
+	}
+
+	void Class::addMember(const ClassMember& _member)
 	{
 		// Checks if a member with the same name does not already exists
-		assert(std::find_if(m_members.begin(), m_members.end(), [_name](const ClassMember& _m) { return strcmp(_name, _m.name) == 0; }) == m_members.end());
+		assert(std::find_if(m_members.begin(), m_members.end(), [_member](const ClassMember& _m) { return strcmp(_member.name, _m.name) == 0; }) == m_members.end());
 
-		m_members.push_back(ClassMember(_name, _address, _type));
+		m_members.push_back(_member);
 	}
 
 	ClassSet::~ClassSet()
 	{
-		for (auto& pair : m_classes)
+		for (auto& pair : m_classesByNameHash)
 		{
 			delete pair.second;
 		}
-		m_classes.clear();
+		m_classesByNameHash.clear();
+		m_classesByTypeHash.clear();
 	}
 
-	mirror::Class* ClassSet::getClass(const char* _className)
+	mirror::Class* ClassSet::getClassByName(const char* _className)
 	{
-		auto it = m_classes.find(HashCString(_className));
-		return it != m_classes.end() ? it->second : nullptr;
+		auto it = m_classesByNameHash.find(HashCString(_className));
+		return it != m_classesByNameHash.end() ? it->second : nullptr;
+	}
+
+	Class* ClassSet::getClassByTypeHash(size_t _classTypeHash)
+	{
+		auto it = m_classesByTypeHash.find(_classTypeHash);
+		return it != m_classesByTypeHash.end() ? it->second : nullptr;
 	}
 
 	void ClassSet::addClass(Class* _class)
 	{
-		// Checks if a class with the same name does not already exists
+		// Checks if a class with the same name/typehash does not already exists
 		assert(_class);
 		uint32_t nameHash = HashCString(_class->getName());
-		assert(m_classes.find(nameHash) == m_classes.end());
+		assert(m_classesByNameHash.find(nameHash) == m_classesByNameHash.end());
+		assert(m_classesByTypeHash.find(_class->getTypeHash()) == m_classesByTypeHash.end());
 
-		m_classes.insert(std::make_pair(nameHash, _class));
+		m_classesByNameHash.insert(std::make_pair(nameHash, _class));
+		m_classesByTypeHash.insert(std::make_pair(_class->getTypeHash(), _class));
 	}
 
 	void ClassSet::removeClass(Class* _class)
 	{
-		// Checks if a class with the same name not already exists
 		assert(_class);
-		uint32_t nameHash = HashCString(_class->getName());
-		auto it = m_classes.find(nameHash);
-		assert(it != m_classes.end());
 
-		m_classes.erase(it);
+		// Checks if a class with the same name/typehash already exists
+		auto it = m_classesByNameHash.find(HashCString(_class->getName()));
+		assert(it != m_classesByNameHash.end());
+		m_classesByNameHash.erase(it);
+
+		auto it2 = m_classesByTypeHash.find(_class->getTypeHash());
+		assert(it2 != m_classesByTypeHash.end());
+		m_classesByTypeHash.erase(it2);
 	}
 }
--- mirror.h
+++ mirror.h
@@ -5,33 +5,52 @@
 #include <vector>
 #include <string.h>
 #include <type_traits>
+#include <typeinfo>
 
 #define MIRROR(_class)\
 public:\
 	static mirror::Class* GetClass()\
 	{\
-		mirror::Class* mClass = mirror::g_classSet.getClass(#_class);\
+		mirror::Class* mClass = mirror::g_classSet.getClassByTypeHash(typeid(_class).hash_code());\
 		if (!mClass)\
 		{\
-			mClass = new mirror::Class(#_class);\
+			mClass = new mirror::Class(#_class, typeid(_class).hash_code());\
+			mirror::g_classSet.addClass(mClass);\
 			char fakePrototype[sizeof(_class)];\
 			_class* prototypePtr = (_class*) fakePrototype;\
 			_MIRROR_CONTENT
 
 #define _MIRROR_CONTENT(...)\
 			__VA_ARGS__\
-			mirror::g_classSet.addClass(mClass); \
 		}\
-		return mClass; \
+		return mClass;\
 	}
 
 #define M_MEMBER(_memberName)\
-	mClass->addMember(#_memberName, reinterpret_cast<size_t>(&(prototypePtr->_memberName)) - reinterpret_cast<size_t>(&prototypePtr), mirror::getMemberType(&(prototypePtr->_memberName)));
+	{\
+		size_t offset = reinterpret_cast<size_t>(&(prototypePtr->_memberName)) - reinterpret_cast<size_t>(&prototypePtr);\
+		mirror::MemberType type = mirror::GetMemberType(prototypePtr->_memberName);\
+		if (type == mirror::MemberType_ClassInstance)\
+		{\
+			mClass->addMember(mirror::ClassMember(#_memberName, offset, type, mirror::GetClass(prototypePtr->_memberName)));\
+		}\
+		else\
+		{\
+			mClass->addMember(mirror::ClassMember(#_memberName, offset, type));\
+		}\
+	}
 
 namespace mirror
 {
+	class Class;
+
 	enum MemberType
 	{
+		MemberType_none,
+
+		MemberType_bool,
+		MemberType_char,
+
 		MemberType_int8,
 		MemberType_int16,
 		MemberType_int32,
@@ -50,42 +69,64 @@ namespace mirror
 		MemberType_fixedCArray,
 		MemberType_dynamicCArray,
 
+		MemberType_ClassInstance,
 	};
-	template <typename T> MemberType getMemberType(T*);
-	MemberType getMemberType(int8_t*);
-	MemberType getMemberType(int16_t*);
-	MemberType getMemberType(int32_t*);
-	MemberType getMemberType(int64_t*);
-	MemberType getMemberType(uint8_t*);
-	MemberType getMemberType(uint16_t*);
-	MemberType getMemberType(uint32_t*);
-	MemberType getMemberType(uint64_t*);
-	MemberType getMemberType(float*);
-	MemberType getMemberType(double*);
-	MemberType getMemberType(std::string*);
+	template <typename T> MemberType GetMemberType(T&);
+	MemberType GetMemberType(bool&);
+	MemberType GetMemberType(char&);
+	MemberType GetMemberType(int8_t&);
+	MemberType GetMemberType(int16_t&);
+	MemberType GetMemberType(int32_t&);
+	MemberType GetMemberType(int64_t&);
+	MemberType GetMemberType(uint8_t&);
+	MemberType GetMemberType(uint16_t&);
+	MemberType GetMemberType(uint32_t&);
+	MemberType GetMemberType(uint64_t&);
+	MemberType GetMemberType(float&);
+	MemberType GetMemberType(double&);
+	MemberType GetMemberType(std::string&);
+
+	template<typename T> Class* GetClass(T&);
+	Class* GetClass(bool&);
+	Class* GetClass(char&);
+	Class* GetClass(int8_t&);
+	Class* GetClass(int16_t&);
+	Class* GetClass(int32_t&);
+	Class* GetClass(int64_t&);
+	Class* GetClass(uint8_t&);
+	Class* GetClass(uint16_t&);
+	Class* GetClass(uint32_t&);
+	Class* GetClass(uint64_t&);
+	Class* GetClass(float&);
+	Class* GetClass(double&);
+	Class* GetClass(std::string&);
 
 	struct ClassMember
 	{
 		ClassMember(const char* _name, size_t _offset, MemberType _type);
+		ClassMember(const char* _name, size_t _offset, MemberType _type, Class* _subClass);
 
 		const char* name;
 		size_t offset;
 		MemberType type;
+		Class* subClass = nullptr;
 	};
 
 
 	class Class
 	{
 	public:
-		Class(const char* _name);
+		Class(const char* _name, size_t _typeHash);
 
 		inline const std::vector<ClassMember>& getMembers() const { return m_members; }
 		inline const char* getName() const { return m_name; }
+		inline size_t getTypeHash() const { return m_typeHash; }
 
-		void addMember(const char* _name, size_t _address, MemberType _type);
+		void addMember(const ClassMember& _member);
 
 	private:
 		std::vector<ClassMember> m_members;
+		size_t m_typeHash;
 		const char* m_name;
 	};
 
@@ -95,13 +136,15 @@ namespace mirror
 	public:
 		~ClassSet();
 
-		Class* getClass(const char* _className);
+		Class* getClassByName(const char* _className);
+		Class* getClassByTypeHash(size_t _classTypeHash);
 
 		void addClass(Class* _class);
 		void removeClass(Class* _class);
 
 	private:
-		std::unordered_map<uint32_t, Class*> m_classes;
+		std::unordered_map<uint32_t, Class*> m_classesByNameHash;
+		std::unordered_map<size_t, Class*> m_classesByTypeHash;
 	};
 	extern ClassSet	g_classSet;
 
@@ -111,4 +154,17 @@ namespace mirror
 }
 
 // INL
-template <typename T> mirror::MemberType mirror::getMemberType(T*) { static_assert(false, "unsupported type"); return 0; }
+template <typename T> mirror::MemberType mirror::GetMemberType(T&)
+{
+	if (T::GetClass())
+	{
+		return MemberType_ClassInstance;
+	}
+	return MemberType_none;
+}
+
+template <typename T> mirror::Class* mirror::GetClass(T&)
+{
+	return T::GetClass();
+}
+
