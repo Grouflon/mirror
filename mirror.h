4bd7ca9b788aa53f369cf8c39ca2d8674bad0a19
berdal84 <contact@dalle-cort.fr>
2021-11-11 14:11:17 -0500

    fix(mirror.h): add MIRROR_DYNAMICALY_LINKED to enable dll import/export (disable by default)

--- mirror.h
+++ mirror.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#ifdef WIN32
-#ifdef MIRROR_EXPORT
-#define MIRROR_API __declspec(dllexport)
-#else
-#define MIRROR_API __declspec(dllimport)
-#endif
-#else
-#define MIRROR_API
+#ifdef MIRROR_DYNAMICALLY_LINKED
+    #ifdef WIN32
+        #ifdef MIRROR_EXPORT
+            #define MIRROR_API __declspec(dllexport)
+        #else
+            #define MIRROR_API __declspec(dllimport)
+        #endif
+    #else
+        #define MIRROR_API
+    #endif
 #endif
 
 #pragma warning( push )
